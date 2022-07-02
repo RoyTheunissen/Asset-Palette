c303cb26e72a6bd0fc19b634c29a781868a634eb
RoyTheunissen <roy.theunissen@live.nl>
2022-07-02 22:36:54 +0200

    Added a system for creating palette assets.

--- Scripts/Editor/PrefabPaletteWindow.cs
+++ Scripts/Editor/PrefabPaletteWindow.cs
@@ -14,6 +14,7 @@ namespace RoyTheunissen.PrefabPalette
     public class PrefabPaletteWindow : EditorWindow
     {
         private const string EditorPrefPrefix = "RoyTheunissen/PrefabPalette/";
+        private const string CurrentCollectionGUIDEditorPref = EditorPrefPrefix + "CurrentCollectionGUID";
         private const string NavigationPanelWidthEditorPref = EditorPrefPrefix + "NavigationPanelWidth";
         private const string ZoomLevelEditorPref = EditorPrefPrefix + "ZoomLevel";
 
@@ -24,8 +25,10 @@ namespace RoyTheunissen.PrefabPalette
         private const float PrefabPanelWidthMin = 200;
         private const float PrefabPanelHeightMin = 50;
         private const float WindowWidthMin = NavigationPanelWidthMin + PrefabPanelWidthMin;
+        
+        private static float HeaderHeight => EditorGUIUtility.singleLineHeight + 3;
         private static float FooterHeight => EditorGUIUtility.singleLineHeight + 6;
-        private static readonly float WindowHeightMin = FooterHeight + PrefabPanelHeightMin;
+        private static readonly float WindowHeightMin = FooterHeight + HeaderHeight + PrefabPanelHeightMin;
         
         private const float DividerBrightness = 0.13f;
         private static readonly Color DividerColor = new Color(DividerBrightness, DividerBrightness, DividerBrightness);
@@ -53,8 +56,27 @@ namespace RoyTheunissen.PrefabPalette
                     {
                         alignment = TextAnchor.LowerCenter
                     };
+                    cachedPrefabPreviewTextStyle.normal.textColor = Color.white;
                 }
                 return cachedPrefabPreviewTextStyle;
+            }
+        }
+        
+        private GUIStyle cachedMessageTextStyle;
+        private bool didCacheMessageTextStyle;
+        private GUIStyle MessageTextStyle
+        {
+            get
+            {
+                if (!didCacheMessageTextStyle)
+                {
+                    didCacheMessageTextStyle = true;
+                    cachedMessageTextStyle = new GUIStyle(EditorStyles.label)
+                    {
+                        alignment = TextAnchor.MiddleCenter
+                    };
+                }
+                return cachedMessageTextStyle;
             }
         }
 
@@ -63,10 +85,62 @@ namespace RoyTheunissen.PrefabPalette
             get
             {
                 if (!EditorPrefs.HasKey(NavigationPanelWidthEditorPref))
-                    NavigationPanelWidth = 200;
-                return EditorPrefs.GetFloat(NavigationPanelWidthEditorPref);
+                    NavigationPanelWidth = NavigationPanelWidthMin;
+                return Mathf.Max(EditorPrefs.GetFloat(NavigationPanelWidthEditorPref), NavigationPanelWidthMin);
             }
             set => EditorPrefs.SetFloat(NavigationPanelWidthEditorPref, value);
+        }
+        
+        private string CurrentCollectionGuid
+        {
+            get => EditorPrefs.GetString(CurrentCollectionGUIDEditorPref);
+            set
+            {
+                if (CurrentCollectionGuid == value)
+                    return;
+                
+                EditorPrefs.SetString(CurrentCollectionGUIDEditorPref, value);
+                
+                // Need to re-cache the current collection now.
+                cachedCurrentCollection = null;
+            }
+        }
+
+        private PrefabPaletteCollection cachedCurrentCollection;
+        private PrefabPaletteCollection CurrentCollection
+        {
+            get
+            {
+                if (cachedCurrentCollection == null)
+                {
+                    string guid = CurrentCollectionGuid;
+                    
+                    if (string.IsNullOrEmpty(guid))
+                        return null;
+
+                    string path = AssetDatabase.GUIDToAssetPath(guid);
+                    if (string.IsNullOrEmpty(path))
+                        return null;
+
+                    cachedCurrentCollection = AssetDatabase.LoadAssetAtPath<PrefabPaletteCollection>(path);
+                }
+
+                return cachedCurrentCollection;
+            }
+            set
+            {
+                if (value == CurrentCollection)
+                    return;
+                
+                if (value == null)
+                {
+                    cachedCurrentCollection = null;
+                    CurrentCollectionGuid = null;
+                    return;
+                }
+
+                CurrentCollectionGuid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(value));
+            }
         }
 
         private float ZoomLevel
@@ -90,6 +164,8 @@ namespace RoyTheunissen.PrefabPalette
 
         private void OnGUI()
         {
+            DrawHeader();
+            
             EditorGUILayout.BeginHorizontal();
             {
                 EditorGUILayout.BeginVertical();
@@ -113,6 +189,75 @@ namespace RoyTheunissen.PrefabPalette
             EditorGUILayout.EndHorizontal();
         }
 
+        private void DrawHeader()
+        {
+            Rect headerRect = GUILayoutUtility.GetRect(position.width, HeaderHeight);
+            GUI.Box(headerRect, GUIContent.none, EditorStyles.toolbar);
+
+            PrefabPaletteCollection currentCollection = CurrentCollection;
+            string name = currentCollection == null ? "[No Collection]" : currentCollection.name;
+            
+            // Allow a new collection to be created or loaded.
+            Rect collectionRect = headerRect.GetSubRectFromLeft(130);
+            bool didClick = GUI.Button(collectionRect, name, EditorStyles.toolbarDropDown);
+            if (didClick)
+                DoCollectionDropDown(collectionRect);
+        }
+
+        private void DoCollectionDropDown(Rect collectionRect)
+        {
+            string[] existingCollectionGuids = AssetDatabase.FindAssets($"t:{typeof(PrefabPaletteCollection).Name}");
+
+            // Allow a new collection to be created.
+            GenericMenu dropdownMenu = new GenericMenu();
+            dropdownMenu.AddItem(new GUIContent("Create New"), false, CreateNewCollection);
+
+            if (existingCollectionGuids.Length > 0)
+                dropdownMenu.AddSeparator("");
+
+            // Add any existing collections that we find as options.
+            foreach (string collection in existingCollectionGuids)
+            {
+                bool isCurrentCollection = collection == CurrentCollectionGuid;
+                string collectionName = Path.GetFileNameWithoutExtension(AssetDatabase.GUIDToAssetPath(collection));
+                dropdownMenu.AddItem(
+                    new GUIContent(collectionName), isCurrentCollection, LoadExistingCollection, collection);
+            }
+
+            dropdownMenu.DropDown(collectionRect);
+        }
+
+        private void CreateNewCollection()
+        {
+            string path = EditorUtility.SaveFilePanel("Create New Prefab Palette", null, "Prefab Palette", "asset");
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            // Make the path relative to the project.
+            string absolutePath = path;
+            path = Path.Combine("Assets", Path.GetRelativePath(Application.dataPath, path));
+
+            Directory.CreateDirectory(path);
+
+            PrefabPaletteCollection newCollection = CreateInstance<PrefabPaletteCollection>();
+            AssetDatabase.CreateAsset(newCollection, path);
+            AssetDatabase.SaveAssets();
+            EditorGUIUtility.PingObject(newCollection);
+
+            CurrentCollection = newCollection;
+            
+            Repaint();
+        }
+
+        private void LoadExistingCollection(object userdata)
+        {
+            string path = (string)userdata;
+            string guid = AssetDatabase.AssetPathToGUID(path);
+            CurrentCollectionGuid = guid;
+            
+            Repaint();
+        }
+
         private void DrawNavigationPanel()
         {
             navigationPanelScrollPosition = EditorGUILayout.BeginScrollView(
@@ -120,7 +265,8 @@ namespace RoyTheunissen.PrefabPalette
             EditorGUILayout.EndScrollView();
 
             const int thickness = 1;
-            Rect dividerRect = new Rect(NavigationPanelWidth - thickness, 0, thickness, position.height);
+            Rect dividerRect = new Rect(
+                NavigationPanelWidth - thickness, HeaderHeight, thickness, position.height);
             EditorGUI.DrawRect(dividerRect, DividerColor);
 
             const int expansion = 1;
@@ -198,11 +344,45 @@ namespace RoyTheunissen.PrefabPalette
         private void DrawPrefabs()
         {
             bool isMouseInPrefabPanel = Event.current.mousePosition.x > NavigationPanelWidth &&
+                                        Event.current.mousePosition.y > HeaderHeight &&
                                         Event.current.mousePosition.y < FooterHeight;
             
             prefabPreviewsScrollPosition = GUILayout.BeginScrollView(prefabPreviewsScrollPosition);
             Rect prefabPanelRect = new Rect(0, 0, position.width - NavigationPanelWidth, 90000000);
             EditorGUI.DrawRect(prefabPanelRect, new Color(0, 0, 0, 0.1f));
+
+            PrefabPaletteCollection currentCollection = CurrentCollection;
+            
+            bool hasCollection = currentCollection != null;
+            bool hasPrefabs = hasCollection && prefabsToDisplay.Count > 0;
+            
+            if (!hasCollection || !hasPrefabs)
+            {
+                GUILayout.FlexibleSpace();
+
+                if (!hasCollection)
+                {
+                    EditorGUILayout.LabelField(
+                        "To begin organizing prefabs, create a collection.", MessageTextStyle);
+                    EditorGUILayout.BeginHorizontal();
+                    {
+                        GUILayout.FlexibleSpace();
+                        bool shouldCreate = GUILayout.Button("Create", GUILayout.Width(100));
+                        GUILayout.FlexibleSpace();
+                        
+                        if (shouldCreate)
+                            CreateNewCollection();
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+                else
+                {
+                    EditorGUILayout.LabelField("Drag prefabs here!", MessageTextStyle);
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndScrollView();
+                return;
+            }
 
             // Needs to be shortened because there must be space for a scrollbar and such...
             float containerWidth = Mathf.Floor(EditorGUIUtility.currentViewWidth) - NavigationPanelWidth - 22;
---
+++ Scripts/PrefabPaletteCollection.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RoyTheunissen.PrefabPalette
+{
+    /// <summary>
+    /// Stores prefab palettes.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Prefab Palette Collection", menuName = "ScriptableObject/Prefab Palette Collection")]
+    public class PrefabPaletteCollection : ScriptableObject 
+    {
+    }
+}
