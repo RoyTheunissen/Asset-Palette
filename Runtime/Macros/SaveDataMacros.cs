b9b97b99aa41c33222e35d471cc68311d4f89384
RoyTheunissen <roy.theunissen@live.nl>
2022-07-19 09:53:20 +0200

    Added some macro code for managing save data.

---
+++ Runtime/Macros/SaveDataMacros.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace RoyTheunissen.AssetPalette.Runtime.Macros
+{
+    /// <summary>
+    /// Macros for dealing with save data.
+    /// </summary>
+    public static class SaveDataMacros 
+    {
+        public static void ResetSaveData()
+        {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorUtility.DisplayDialog(
+                "Reset Save Data", "Are you sure you want to reset your save data?", "Yes"))
+            {
+                PlayerPrefs.DeleteAll();
+            }
+#endif
+        }
+        
+        public static void ResetEditorSaveData()
+        {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorUtility.DisplayDialog(
+                "Reset Editor Save Data", "Are you sure you want to reset your editor save data?" +
+                    "This may reset a lot of preferences for Unity and its extensions.",
+                "Yes, I know what I'm doing."))
+            {
+                UnityEditor.EditorPrefs.DeleteAll();
+            }
+#endif
+        }
+    }
+}
