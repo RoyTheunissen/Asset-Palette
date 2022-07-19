50733e5d34e89068ef4cb28c314fcfd38aa9ad52
RoyTheunissen <roy.theunissen@live.nl>
2022-07-19 12:02:34 +0200

    Entry drawers now support inheritance.

--- Editor/CustomEditors/PaletteDrawing.cs
+++ Editor/CustomEditors/PaletteDrawing.cs
@@ -53,24 +53,42 @@ namespace RoyTheunissen.AssetPalette.Editor.CustomEditors
             return newEntryDrawerInstance;
         }
 
-        private static Type GetEntryDrawerType(Type entryType)
+        private static Type[] GetEntryDrawerTypeCandidates(Type entryType)
         {
             Type genericEntryDrawerType = typeof(PaletteEntryDrawer<>);
             Type specificEntryDrawerBaseType = genericEntryDrawerType.MakeGenericType(entryType);
-            Type[] entryDrawerCandidates = specificEntryDrawerBaseType.GetAllAssignableClasses(false);
+            return specificEntryDrawerBaseType.GetAllAssignableClasses(false);
+        }
+        
+        private static Type GetEntryDrawerType(Type entryType)
+        {
+            // Try to find a drawer for this exact entry type. Otherwise, keep trying to find a drawer for its base
+            // classes instead until we reach PaletteEntry and there are no more base classes that could have drawers.
+            Type[] entryDrawerCandidates = new Type[0];
+            Type typeToFindAnEntryDrawerFor = entryType;
+            while (entryDrawerCandidates.Length == 0 && typeToFindAnEntryDrawerFor != null &&
+                   typeToFindAnEntryDrawerFor != typeof(PaletteEntry))
+            {
+                entryDrawerCandidates = GetEntryDrawerTypeCandidates(typeToFindAnEntryDrawerFor);
+                
+                // If we still didn't find any, continue on with the base type.
+                typeToFindAnEntryDrawerFor = typeToFindAnEntryDrawerFor.BaseType;
+            }
+
+            string typeName = entryType.Name;
 
             if (entryDrawerCandidates.Length == 0)
             {
-                Debug.LogError($"No drawer found for palette entry type '{entryType}'." +
-                               $"Try creating a drawer that inherits from PaletteEntryDrawer<{entryType.Name}>");
+                Debug.LogError($"No drawer found for palette entry type '{entryType}'. " +
+                               $"Try creating a drawer that inherits from PaletteEntryDrawer<{typeName}> " +
+                               $"or make sure that {typeName} inherits from a class that has a valid drawer.");
                 return null;
             }
 
             if (entryDrawerCandidates.Length > 1)
             {
-                Debug.LogWarning($"Multiple drawers found for palette entry type '{entryType}'." +
-                                 $"Try making sure there's one PaletteEntryDrawer<{entryType.Name}> for this type." +
-                                 $"Or, alternatively, add more robust inheritance support for entry drawers.");
+                Debug.LogWarning($"Multiple drawers found for palette entry type '{entryType}'. " +
+                                 $"Try making sure there's one PaletteEntryDrawer<{typeName}> for this type.");
             }
 
             return entryDrawerCandidates[0];
