a51f5c123f4ac5b4f1283cf7f7a7a9c4180dbb17
Roy Theunissen <roy.theunissen@live.nl>
2023-08-11 17:46:57 +0200

    Improved a description.

--- Editor/Windows/AssetPaletteAssetImporter.cs
+++ Editor/Windows/AssetPaletteAssetImporter.cs
@@ -4,7 +4,9 @@ using UnityEditor;
 namespace RoyTheunissen.AssetPalette.Windows
 {
     /// <summary>
-    /// Finds out if a palette asset was importer.
+    /// Finds out if a currently viewed palette asset was imported. If so we need to notify the window so it can
+    /// correctly update its treeview for the folders. Otherwise if you discard folder changes in Git then the folders
+    /// will not correctly update accordingly.
     /// </summary>
     public class AssetPaletteAssetImporter : AssetPostprocessor
     {
