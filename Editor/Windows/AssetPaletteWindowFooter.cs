34c162669dfd28aece078ea28fdc135171eee10c
Roy Theunissen <roy.theunissen@live.nl>
2023-08-14 16:43:58 +0200

    The footer now shows the path of the currently selected asset.

--- Editor/Windows/AssetPaletteWindowFooter.cs
+++ Editor/Windows/AssetPaletteWindowFooter.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,6 +18,8 @@ namespace RoyTheunissen.AssetPalette.Windows
             }
             set => EditorPrefs.SetFloat(ZoomLevelEditorPref, value);
         }
+        
+        private List<Object> entryAssetsWhosePathToShow = new List<Object>();
 
         private bool IsZoomLevelFocused => GUI.GetNameOfFocusedControl() == ZoomLevelControlName;
         
@@ -33,6 +36,29 @@ namespace RoyTheunissen.AssetPalette.Windows
                 GUILayout.FlexibleSpace();
                 EditorGUILayout.BeginHorizontal();
                 {
+                    // Draw the asset path of the first asset that is selected. That's how the Project View does it.
+                    foreach (PaletteEntry paletteEntry in entriesSelected)
+                    {
+                        entryAssetsWhosePathToShow.Clear();
+                        paletteEntry.GetAssetsToSelect(ref entryAssetsWhosePathToShow);
+                        if (entryAssetsWhosePathToShow.Count > 0)
+                        {
+                            Object objectToShow = entryAssetsWhosePathToShow[0];
+                            string path = AssetDatabase.GetAssetPath(objectToShow);
+                            Texture icon = 
+                                //EditorGUIUtility.GetIconForObject(objectToShow)
+                                AssetDatabase.GetCachedIcon(path)
+                                ;
+                            GUIContent guiContent = new GUIContent(path, icon);
+                            //EditorGUILayout.LabelField(guiContent);
+                            EditorGUIUtility.SetIconSize(Vector2.one * 14);
+                            Rect pathRect = GUILayoutUtility.GetRect(guiContent, EditorStyles.label);
+                            EditorGUI.LabelField(pathRect, guiContent);
+                            EditorGUIUtility.SetIconSize(Vector2.zero);
+                            break;
+                        }
+                    }
+                    
                     GUILayout.FlexibleSpace();
                     Rect zoomLevelRect = GUILayoutUtility.GetRect(80, EditorGUIUtility.singleLineHeight);
 
