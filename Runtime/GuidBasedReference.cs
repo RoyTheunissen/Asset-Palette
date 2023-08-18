471e881e6f26efe0a02964d1ef7afc75760075e6
Roy Theunissen <roy.theunissen@live.nl>
2023-08-18 13:11:50 +0200

    Minor cleanup of the guid based reference.

--- Runtime/GuidBasedReference.cs
+++ Runtime/GuidBasedReference.cs
@@ -7,6 +7,16 @@ using Object = UnityEngine.Object;
 
 namespace RoyTheunissen.AssetPalette
 {
+    /// <summary>
+    /// There's two reasons why you should be using this class instead of having direct Object references:
+    /// 
+    /// - This way the actual loading of the assets is delayed as much as possible. The palette will not load any of
+    /// the references until it's necessary, for example for generating a preview or actually using a palette entry.
+    /// 
+    /// - This way the Personal Palette can correctly serialize and deserialize the asset reference. The Personal
+    /// Palette is not stored in an asset but is stored in the EditorPrefs as JSON instead. By default Unity's
+    /// JSON utility does not seem to deserialize asset references correctly and they will become corrupt.
+    /// </summary>
     [Serializable]
     public class GuidBasedReference<T>  where T : Object
     {
